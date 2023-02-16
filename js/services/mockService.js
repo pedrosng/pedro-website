dc4fea11bb1844a61e5c4b938643de15facdb528
pedro goncalves <pedro.sng@gmail.com>
2023-02-16 17:39:24 +0000

    adds info view content

--- js/services/mockService.js
+++ js/services/mockService.js
@@ -4,8 +4,23 @@ let homeData = {
     heather: 'Hi there, I´m Pedro!',
     body: 'I´m a fullstack developer based in Porugal. I believe code can help us evolve towards a better world by crafting functionalities that reduce both our ecological footprint and the existing gaps between us humans. I am also a passionate climber and a chronic curious. When not writting code you can find me among the mountains going up granite and limestone walls or immersed on learning something new.'
 }
+
+let infoData = {
+    gitHub: {
+        text: 'GitHub',
+        link:  'https://github.com/pedrosng'
+    },   
+    linkedIn: {
+        text: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/pedronunesgoncalves/' 
+    },
+    instagram: {
+        text: 'Instagram',
+        link: 'https://www.instagram.com/pedro.sng/'   
+    }
+}
+
 let contactData = 'display contact form';
-let infoData = 'git hub, linked in, instagram';
 let projectsData = 'wow so many amazing projects';
 let skillsData = 'my technique is very necessary';
 
--- js/views/info-view.js
+++ js/views/info-view.js
@@ -1,3 +1,5 @@
+import '../../css/info-view.css'
+
 let externals = {}
 
 externals.render = data => {
@@ -5,7 +7,17 @@ externals.render = data => {
 };
 
 const loadContent = data => {
-    $('.content').append('<div id="info">' + data + '</div>');
+    $('.content').append(
+        '<div id="info">' + 
+        '</div>'
+    );
+    loadInfoData(data);
+}
+
+const loadInfoData = data => { 
+    Object.keys(data).forEach(element => {
+        $('#info').append('<a href=' + data[element].link + ' target= "_blank">' + data[element].text + '</a>' + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/></svg>' + '</br>' )
+    });
 }
 
 export default externals;
